3ca35e0d3cfc1938de8c1abe45b6c17e49060353
Shashi Bhushan <38137046+shashi1595@users.noreply.github.com>
2020-01-16 03:19:14 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,87 +1,20 @@
-import React, { Component } from "react";
-import "./styles.css";
-var a = 0,
-  b = 5;
-var i = 0;
-class App extends Component {
+import React from "react";
+
+var a = 5,
+  b = 0;
+
+export default class myTable extends React.Component {
+  createTable = () => {
+    let table = [];
+    for (let i = 1; i <= 10; i++) {
+      table.push(<td>{` 5 X ${i} = ${5 * i} `}</td>);
+
+      table.push(<tr />);
+    }
+    return table;
+  };
+
   render() {
-    return (
-      <div>
-        <h1 className="emp">Multiplication Table of 5</h1>
-        <table className="table">
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-          <tr>
-            <td>{b}</td>
-            <td>X</td>
-            <td>{++a}</td>
-            <td>=</td>
-            <td>{a * b}</td>
-          </tr>
-        </table>
-      </div>
-    );
+    return <table>{this.createTable()}</table>;
   }
 }
-export default App;
